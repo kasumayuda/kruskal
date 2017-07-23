679b6838a11c70c2ffa9228d30889713a1dc112e
kasumayuda <kasuma.yuda@gmail.com>
2017-07-23 21:31:35 +0700

    minor update

---
+++ src/Kruskal/Edge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kruskal
+{
+    public class Edge
+    {
+        public int source;
+        public int destination;
+        public int weight;
+
+        public int ComparetTo(Edge compareTo)
+        {
+            return this.weight - compareTo.weight;
+        }
+
+    }
+}
